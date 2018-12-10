c8b09ecebac106ec27655d0f91a70ce8b47ec40d
Colin Smith <colin@colinsmithdesign.ca>
2018-12-10 00:58:32 -0500

    Add support for multiple root template compiling

--- commands/build.js
+++ commands/build.js
@@ -2,6 +2,7 @@ const consola = require('consola')
 const fs = require('fs-extra')
 const path = require('path')
 const getTestData = require('../lib/get-test-data')
+const getTemplates = require('../lib/get-templates')
 const renderMJML = require('../lib/render-mjml')
 const renderNunjucks = require('../lib/render-nunjucks')
 
@@ -9,42 +10,51 @@ const renderNunjucks = require('../lib/render-nunjucks')
  * Build and write mail template
  * @param {Object} options Function options
  * @param {String} options.templatePath Path of MJML template
- * @param {String} options.outputPath Path for HTML output
+ * @param {String} localOutputPath Path for HTML output
  */
 function build (options) {
   consola.info('Rendering MJML…')
-
-  const mjmlOutput = renderMJML({ path: options.templatePath })
-
-  if (mjmlOutput.errors.length) {
-    consola.error(mjmlOutput.errors)
-    process.exit(1)
-  }
-
-  const testData = getTestData({
-    test: options.test,
-    layout: options.layout
-  })
   
-  const nunjucksOutput = renderNunjucks({
-    template: mjmlOutput.html,
-    context: testData
-  })
+  const templates = getTemplates(options);
 
-  consola.success('MJML rendered.')
+  for (let i = 0; templates.length > i; i++) {
+    consola.info('Rendering: ' + templates[i]);
 
-  consola.info('Writing HTML file and copying attachments…')
+    let localTemplatePath = path.join( path.dirname(options.templatePath), templates[i] );
+    let localOutputPath = path.join( path.dirname(options.templatePath), '../../dist/', path.basename(templates[i], '.mjml') + '.html' );
 
-  try {
-    fs.ensureFileSync(options.outputPath)
-    fs.writeFileSync(options.outputPath, nunjucksOutput)
-    fs.copySync(path.join(process.cwd(), `${options.layout}/src/attachments`), path.dirname(options.outputPath))
-  } catch (error) {
-    consola.error(error.message)
-    process.exit(1)
+    const mjmlOutput = renderMJML({ path: localTemplatePath })
+
+    if (mjmlOutput.errors.length) {
+      consola.error(mjmlOutput.errors)
+      process.exit(1)
+    }
+
+    const testData = getTestData({
+      test: options.test,
+      layout: options.layout
+    })
+    
+    const nunjucksOutput = renderNunjucks({
+      template: mjmlOutput.html,
+      context: testData
+    })
+
+    consola.success('MJML rendered.')
+
+    consola.info('Writing HTML file and copying attachments…')
+
+    try {
+      fs.ensureFileSync(localOutputPath)
+      fs.writeFileSync(localOutputPath, nunjucksOutput)
+      fs.copySync(path.join(process.cwd(), `${options.layout}/src/attachments`), path.dirname(localOutputPath))
+    } catch (error) {
+      consola.error(error.message)
+      process.exit(1)
+    }
+
+    consola.success('HTML written and attachments copied.')
   }
-
-  consola.success('HTML written and attachments copied.')
 }
 
 module.exports = build
